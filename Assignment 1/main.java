9ccd6345b520e2e71060630c44317606d9d6de14
tbreaton <thomas.breaton1@marist.edu>
2023-10-03 21:53:42 -0400

    Commit 2
    Fixed the issue with the stack
    Was able to find all 12 palindromes

--- Assignment 1/main.java
+++ Assignment 1/main.java
@@ -10,21 +10,23 @@ public class Main{
     public static void main(String[] args) throws FileNotFoundException{
         File magicItemsFile = new File("/Users/Tom/Documents/Github/Algorithms/magicitems.txt");
         String[] magicItemsArray = fileToArray(magicItemsFile);
-        /*for (int i=0; i<magicItemsArray.length; i++){
-            System.out.println(magicItemsArray[i]);
-        }*/
         String[] palindromes = findPalindrome(magicItemsArray);
-        /*for(int i=0; i<palindromes.length; i++){
+
+        //Loops through the palindromes array and displays them
+        for(int i=0; i<palindromes.length; i++){
             System.out.println(palindromes[i]);
-        }*/
+        }
     }
 
     public static String[] fileToArray(File file) throws FileNotFoundException{
         
         Scanner scan = new Scanner(file);
 
+        //Creates the array to store the magic itmes in
         String[] itemsArray = new String[666];
         int i=0;
+
+        //Goes through the whole text file and adds each line to the array while converting them to lower case
         while(scan.hasNextLine()){
             String data = scan.nextLine();
             data = data.toLowerCase();
@@ -42,31 +44,35 @@ public class Main{
         Queue<String> queue = new LinkedList<>();
         Stack<String> stack = new Stack<>();
         
-        for (int i=0; i<1; i++){
+        //Loops through the array inputed and returns the 12 palindromes
+        for (int i=0; i<arr.length; i++){
             boolean isPalindrome = false;
+
+            //Adds each character to a queue and stack
             for (int j=0; j<arr[i].length(); j++){
                 if (arr[i].charAt(j) != ' '){
                     String temp = Character.toString(arr[i].charAt(j));
-                    //System.out.println(temp);
                     queue.add(temp);
                     stack.push(temp);
                 }
             }
             int queueLength = queue.size();
-            for (int l=0; l<queueLength; l++){
-                        System.out.println(queue.remove());
-                        //System.out.println(stack.pop());
-                    }
+            //Loops through the queue and stack chcecking if the string is a palindrome
             for (int k=0; k<queueLength; k++){
-                String tempForQueue = queue.remove();
+                String tempForQueue = queue.poll();
                 String tempForStack = stack.pop();
-                if (tempForQueue == tempForStack){
+                if (tempForQueue.compareTo(tempForStack) == 0){
+                    //If all the letters match up, then a palindrome is found
                     isPalindrome = true;
                 }else{
+                    //If the letters don't match up, the loop is broken and the queue and stack are cleared for another string to be entered
                     isPalindrome = false;
+                    queue.clear();
+                    stack.clear();
                     break;
                 }
             }
+            //Adds the palindromes to an array that is returned to the main function
             if (isPalindrome){
                 palindromes[palindromesCounter] = arr[i];
                 palindromesCounter++;
