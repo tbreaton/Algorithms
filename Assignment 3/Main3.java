60c6c251c223f63deaa1b9ca0803464258385321
tbreaton <thomas.breaton1@marist.edu>
2023-11-17 19:55:37 -0500

    Push 6
    Added graphs1 file
    Added Graph class
    Added GraphNode class withing Graph
    Hardcoded to check if the Linked Objects and Adjacency List work
    Now have to work on reading from file and creating matrix

--- Assignment 3/Main3.java
+++ Assignment 3/Main3.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main3 {
 
@@ -34,6 +35,28 @@ public class Main3 {
         int avg = sum / 42;
         System.out.println("Average comparisons for BST search: " + avg);
 
+        File graphsOne = new File("graphs1.txt");
+        Graph g = new Graph();
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addVertex(3);
+        g.addVertex(4);
+        g.addVertex(5);
+        g.addVertex(6);
+        g.addVertex(7);
+        g.addEdge(1, 2);
+        g.addEdge(1, 5);
+        g.addEdge(1, 6);
+        g.addEdge(2, 3);
+        g.addEdge(2, 5);
+        g.addEdge(2, 6);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+        g.addEdge(5, 6);
+        g.addEdge(5, 7);
+        g.addEdge(6, 7);
+
+        g.printGraph();
     }
 
     public static String[] fileToArray(File file) throws FileNotFoundException {
@@ -146,3 +169,86 @@ class BST_class {
 
     }
 }
+
+class Graph {
+    ArrayList<GraphNode> graph;
+    int v;
+
+    class GraphNode {
+
+        private int vertexID = 0;
+        private boolean processed;
+        private ArrayList<Integer> neighbors;
+
+        public GraphNode(int newID) {
+            vertexID = newID;
+            processed = false;
+            neighbors = new ArrayList<Integer>();
+        }
+
+    }
+
+    Graph() {
+        graph = new ArrayList<GraphNode>();
+    }
+
+    void addVertex(int vertexID) {
+        GraphNode vertex = new GraphNode(vertexID);
+        addToGraph(vertex);
+    }
+
+    void addToGraph(GraphNode newVertex) {
+        graph.add(newVertex);
+    }
+
+    GraphNode getVertexByID(int ID) {
+        for (int i = 0; i < graph.size(); i++) {
+            if (graph.get(i).vertexID == ID) {
+                return graph.get(i);
+            }
+        }
+        return null;
+    }
+
+    GraphNode getByID(int ID) {
+        GraphNode result = getVertexByID(ID);
+        return result;
+    }
+
+    void addEdge(int source, int dest) {
+        GraphNode tempSource = getVertexByID(source);
+        GraphNode tempDest = getVertexByID(dest);
+
+        tempSource.neighbors.add(dest);
+        tempDest.neighbors.add(source);
+    }
+
+    void printGraph() {
+        for (int i = 0; i < graph.size(); i++) {
+            GraphNode tempVertex = graph.get(i);
+            System.out.print("Vertex " + tempVertex.vertexID + " has neighbors: ");
+            for (int k = 0; k < tempVertex.neighbors.size(); k++) {
+                System.out.print(tempVertex.neighbors.get(k) + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+ * void fileToGraph(File file) throws FileNotFoundException {
+ * Scanner scan = new Scanner(file);
+ * 
+ * int i = 0;
+ * 
+ * while (scan.hasNextLine()) {
+ * String data = scan.nextLine();
+ * String delims = "[ ]+";
+ * String[] tempString = data.split(delims);
+ * if (tempString[0].compareTo("new") == 0 || tempString[0].compareTo("--") ==
+ * 0) {
+ * 
+ * }
+ * }
+ * }
+ */
